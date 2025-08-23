79207aa1f84c85913f01e60ae5fd08974bb563e6
Székely Ádám <37347567+deviladam@users.noreply.github.com>
2025-08-24 00:31:54 +0200

    feat: remove wormholes after mop

--- Data/Wormholes.lua
+++ Data/Wormholes.lua
@@ -9,13 +9,13 @@ tpm.Wormholes = {
 	[30544] = true, -- Ultrasafe Transporter: Toshley's Station
 	[48933] = true, -- Wormhole Generator: Northrend
 	[87215] = true, -- Wormhole Generator: Pandaria
-	[112059] = true, -- Wormhole Centrifuge (Dreanor) 6
-	[151652] = true, -- Wormhole Generator: Argus
-	[168807] = true, -- Wormhole Generator: Kul Tiras 5
-	[168808] = true, -- Wormhole Generator: Zandalar
-	[172924] = true, -- Wormhole Generator: Shadowlands 3
-	[198156] = true, -- Wyrmhole Generator: Dragon Isles 4
-	[221966] = true, -- Wormhole Generator: Khaz Algar
+	-- [112059] = true, -- Wormhole Centrifuge (Dreanor) 6
+	-- [151652] = true, -- Wormhole Generator: Argus
+	-- [168807] = true, -- Wormhole Generator: Kul Tiras 5
+	-- [168808] = true, -- Wormhole Generator: Zandalar
+	-- [172924] = true, -- Wormhole Generator: Shadowlands 3
+	-- [198156] = true, -- Wyrmhole Generator: Dragon Isles 4
+	-- [221966] = true, -- Wormhole Generator: Khaz Algar
 }
 
 function tpm:UpdateAvailableWormholes()
